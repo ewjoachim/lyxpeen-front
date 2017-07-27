d7c43f84b0f4138668ce03cfc30d94b2b5d66e58
Joachim Jablon <ewjoachim@gmail.com>
2017-07-27 23:21:27 +0200

    main > isMain

--- app/models/singer-part.js
+++ app/models/singer-part.js
@@ -1,7 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  main: DS.attr('boolean'),
+  isMain: DS.attr('boolean'),
   songPart: DS.belongsTo('song-part'),
   singer: DS.belongsTo('singer'),
 });
