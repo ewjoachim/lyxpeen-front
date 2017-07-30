c88bbe447856664af6e0cd911e29c149326c0580
Joachim Jablon <ewjoachim@gmail.com>
2017-07-30 17:57:12 +0200

    Remove stuff

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -4,11 +4,6 @@ import Ember from 'ember';
 export default DS.RESTAdapter.extend({
   namespace: "/api",
 
-  buildURL: function(type, id, record) {
-    //call the default buildURL and then append a slash
-    return this._super(type, id, record) + '/';
-  },
-
   pathForType: function(modelName) {
     var decamelized = Ember.String.underscore(modelName);
     return Ember.String.pluralize(decamelized);
