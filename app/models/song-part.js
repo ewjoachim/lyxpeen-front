6c28efc8877c918b96de2a30e46949fc570476f3
Joachim Jablon <ewjoachim@gmail.com>
2017-08-05 12:43:00 +0200

    Sort singer parts

--- app/models/song-part.js
+++ app/models/song-part.js
@@ -1,8 +1,13 @@
+import Ember from 'ember';
 import DS from 'ember-data';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
   song: DS.belongsTo('song'),
   section: DS.belongsTo('section'),
-  singerParts: DS.hasMany('singer-part')
+  singerParts: DS.hasMany('singer-part'),
+
+  singerPartsSorting: ["isMainPart:desc"],
+  sortedSingerParts: Ember.computed.sort("singerParts", "singerPartsSorting"),
+
 });
