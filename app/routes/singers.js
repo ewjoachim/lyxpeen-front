4f17e51079648c7cfad88f1c4a4c982ee58ccdda
Joachim Jablon <ewjoachim@gmail.com>
2017-08-06 12:33:46 +0200

    Explain the code

--- app/routes/singers.js
+++ app/routes/singers.js
@@ -5,6 +5,8 @@ export default Ember.Route.extend({
     let result;
     return this.store.findAll('singer').then((singers)=>{
       result = singers
+      // Before we return the list of all singers, make sure their
+      // section is loaded.
       return Ember.RSVP.Promise.all(singers.getEach("mainSection"));
     }).then(()=>result);
   },
