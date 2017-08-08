9debb7f9306b0e163251ac9191ddd1c334ac7ee5
Joachim Jablon <ewjoachim@gmail.com>
2017-08-08 23:39:05 +0200

    Simplify models in singers view

--- app/routes/singers.js
+++ app/routes/singers.js
@@ -2,12 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    let result;
-    return this.store.findAll('singer').then((singers)=>{
-      result = singers
-      // Before we return the list of all singers, make sure their
-      // section is loaded.
-      return Ember.RSVP.Promise.all(singers.getEach("mainSection"));
-    }).then(()=>result);
+    return this.store.findAll('singer')
   },
+  afterModel(singers) {
+    return Ember.RSVP.Promise.all(singers.getEach("mainSection"))
+  }
 });
