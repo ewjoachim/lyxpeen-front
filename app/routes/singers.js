ebecdd2ff8b256138f64d52f5a9299cd37471772
Joachim Jablon <ewjoachim@gmail.com>
2017-08-12 11:49:25 +0200

    Better load models (1 request instead of n)

--- app/routes/singers.js
+++ app/routes/singers.js
@@ -2,9 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.findAll('singer')
+    return this.store.findAll('singer');
   },
   afterModel(singers) {
-    return Ember.RSVP.Promise.all(singers.getEach("mainSection"))
-  }
+    return Ember.RSVP.hash({
+      sections: this.store.findAll('section'),
+      users: this.store.findAll('user'),
+    });
+  },
 });
