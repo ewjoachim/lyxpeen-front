6554e66992b707fa1028b8ebe8ba6b4b372c1f2d
Joachim Jablon <ewjoachim@gmail.com>
2017-08-12 11:48:44 +0200

    Sort by username too

--- app/controllers/singers.js
+++ app/controllers/singers.js
@@ -1,6 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  singersSorting: ["isActive:desc", "mainSection.orderKey"],
+  singersSorting: ["isActive:desc", "mainSection.orderKey", "user.name"],
   sortedSingers: Ember.computed.sort("model", "singersSorting"),
 });
